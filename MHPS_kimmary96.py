7cddcc3c4d1ae93b0128d3a43804f656f8fc0a4a
kimmary96 <kimgone125@gmail.com>
2025-12-31 17:10:45 +0900

    feat: Fix number 1000 to 100000

--- MHPS_kimmary96.py
+++ MHPS_kimmary96.py
@@ -5,7 +5,7 @@ import random
 stay = 0  #amount won if stay in the same position
 switch = 0 # amount won if you switch 
 
-for i in range(1000):
+for i in range(100000):
     lst = [1,0,0]           # one car and two goats
     random.shuffle(lst)     # shuffles the list randomly
     
